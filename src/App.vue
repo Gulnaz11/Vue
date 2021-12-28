adf21a11934735f1c179347b46ca7e973ae8246a
Gulnaz11 <81852862+Gulnaz11@users.noreply.github.com>
2021-12-28 20:44:27 +0300

    create calculator

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,54 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="increase(2,$event)">  Increase</button>
+    <button @click="restCounter">Delete</button>
+    {{counter}}
+    <label for="test">test
+    <input
+      id="test"
+      name=""
+      type="test"
+      @blur="blur"
+      @focus="focus"
+    >
+    </label>
+    <CalculatorComp  />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import CalculatorComp from './components/Calc.vue';
 
 export default {
   name: 'App',
   components: {
+    CalculatorComp,
     HelloWorld,
+  },
+  data: () => ({
+    counter: 0,
+  }),
+  methods: {
+    increase(step, event) {
+      this.counter += step;
+      console.log(event);
+    },
+    restCounter() {
+      this.counter = 0;
+    },
+    blur() {
+      console.log('blur');
+    },
+    focus() {
+      console.log('focus');
+    },
   },
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Calc.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <div class="main">
+    <label for="op1">
+      <input type="number" id="op1" v-model.number="op1">
+    </label>
+    <label for="op2">
+      <input type="number" id="op2" v-model.number="op2">
+    </label>
+    = {{result}}
+  </div>
+  <div class="keyboard">
+    <button @click="result=op1+ op2">+</button>
+    <button @click="sub">-</button>
+    <button @click="div">/</button>
+    <button @click="mult">*</button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'CalculatorComp',
+  data: () => ({
+    result: 0,
+    op1: 0,
+    op2: 0,
+  }),
+  methods: {
+    sub() {
+      this.result = this.op1 - this.op2;
+    },
+    div() {
+      this.result = this.op1 / this.op2;
+    },
+    mult() {
+      this.result = this.op1 * this.op2;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
