62c4b811a07dee5635c08d03fd98f323c78ae30f
Gulnaz11 <81852862+Gulnaz11@users.noreply.github.com>
2022-01-17 18:35:52 +0300

    Add PaymentDisplay.vue and AddPaymentForm.vue (repeate from lesson3)

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,63 @@
 <template>
   <div id="app">
-    <MyCounter v-if="show"/>
-    <button @click="show=!show">hide / show</button>
+<!--    <MyCounter v-if="show"/>-->
+<!--    <button @click="show=!show">hide / show</button>-->
+    <header>
+      <div class="header">My personal costs</div>
+    </header>
+
+    <main>
+       <PaymentDisplay class="PaymentDisplay" :items="paymentsList"  />
+      <AddPaymentForm @add-payment="addPayment"/>
+    </main>
+
   </div>
 </template>
 
 <script>
-import MyCounter from './components/Counter.vue';
+// import MyCounter from './components/Counter.vue';
+
+import PaymentDisplay from './components/PaymentDisplay.vue';
+import AddPaymentForm from './components/AddPaymentForm.vue';
 
 export default {
   name: 'App',
   components: {
-    MyCounter,
+    AddPaymentForm,
+    PaymentDisplay,
+    // MyCounter,
   },
   data: () => ({
     counter: 0,
     show: true,
+    paymentsList: [],
   }),
   methods: {
-    increase(step, event) {
-      this.counter += step;
-      console.log(event);
+    fetchData() {
+      return [
+        {
+          date: '17.01.2022',
+          category: 'Food',
+          value: 300,
+        },
+        {
+          date: '18.01.2022',
+          category: 'Food',
+          value: 500,
+        },
+        {
+          date: '18.01.2022',
+          category: 'Transport',
+          value: 200,
+        },
+      ];
     },
-    restCounter() {
-      this.counter = 0;
+    addPayment(data) {
+      this.paymentsList.push(data);
     },
-    blur() {
-      console.log('blur');
-    },
-    focus() {
-      console.log('focus');
-    },
+  },
+  created() {
+    this.paymentsList = this.fetchData();
   },
 };
 </script>
---
+++ src/components/AddPaymentForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <input type="text" placeholder="Amount" v-model="value">
+    <input type="text" placeholder="Type" v-model="category">
+    <input type="text" placeholder="Date" v-model="date">
+    <button @click="addPayment">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddPaymentForm',
+  data: () => ({
+    value: '',
+    category: '',
+    date: '',
+  }),
+  methods: {
+    addPayment() {
+      // const data = {
+      //   value: this.amount,
+      //   category: this.type,
+      //   date: this.date || this.paymentDay,
+      // };
+      const {
+        value, category, date, paymentDay,
+      } = this;
+      const data = {
+        value,
+        category,
+        date: date || paymentDay,
+      };
+      this.$emit('add-payment', data);
+    },
+  },
+  computed: {
+    paymentDay() {
+      const currentDate = new Date();
+      const day = currentDate.getDay();
+      const month = currentDate.getMonth() + 1;
+      const year = currentDate.getFullYear();
+      return `${day}.${month}.${year}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
