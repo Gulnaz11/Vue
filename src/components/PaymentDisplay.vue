3e1bd2e0e858837335f442fca87698755d936d5d
Gulnaz11 <81852862+Gulnaz11@users.noreply.github.com>
2022-02-01 17:57:23 +0300

    add context menu

--- src/components/PaymentDisplay.vue
+++ src/components/PaymentDisplay.vue
@@ -37,6 +37,7 @@
         <v-col cols="4">Date</v-col>
         <v-col cols="4">Category</v-col>
         <v-col cols="2">Value</v-col>
+        <v-col cols="1"></v-col>
       </v-row>
       <v-row
         v-for="(item, index) in items[1]"
@@ -47,13 +48,26 @@
         <v-col>{{ item.category}}</v-col>
         <v-col>{{ item.value}}</v-col>
         <v-col>
-          <i class="fas fa-ellipsis-v" @click="showMenu[item.id]=!showMenu[item.id]">-->
-          </i>
-          <div v-show="showMenu[item.id]" class="content">
-          <UpdateData class="Menu" :data="item"/>
-          <DeleteData class="Menu" :data="item"/>
-          </div>
-        </v-col>
+          <v-menu
+          bottom
+          left
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              dark
+              icon
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon color="black">mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <UpdateData class="Menu" :data="item"/>
+            <DeleteData class="Menu" :data="item"/>
+          </v-list>
+        </v-menu></v-col>
+
       </v-row>
     </v-container>
     <nav aria-label="Page navigation example">
