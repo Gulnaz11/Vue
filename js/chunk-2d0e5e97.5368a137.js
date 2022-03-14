c4cb0845f841c3aa2d208f46d0a4b1a6563d29b1
Gulnaz11 <81852862+Gulnaz11@users.noreply.github.com>
2022-03-14 15:11:50 +0300

    Updates

---
+++ js/chunk-2d0e5e97.5368a137.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5e97"],{9703:function(n,e,t){"use strict";t.r(e);var o=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" Page not found ")])},u=[],a={name:"NotFound"},c=a,r=t("2877"),s=Object(r["a"])(c,o,u,!1,null,"04ac5761",null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0e5e97.5368a137.js.map
---
+++ js/chunk-2d22d746.6e989cef.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" About Page ")])},u=[],a=t("2877"),o={},r=Object(a["a"])(o,c,u,!1,null,"5c4ad350",null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.6e989cef.js.map
